b6fe103bc4291b741ea37b530bfa567438093921
Anastasiia Inzer <anastasiia.inzer@uklon.com.ua>
2025-03-17 22:34:05 +0200

    Power of a number №2

--- homeworkLoop.js
+++ homeworkLoop.js
@@ -1,10 +1,28 @@
 
-let result = true;
+// Перший варіант залишу. Прочитала завдання, але зрозуміла по-своєму його
 
-function powerOfNumber(number = 5, power = 3) {
+/*function powerOfNumber(number = 5, power = 3) {
   
     result = Math.pow(number, power)
 }
 
 powerOfNumber(); // Виклик функції без аргументів
 console.log(result);
+*/
+
+//______________________________________________________________//
+
+// Другий варіант, де не використовуємо вбудовані функції і **
+function pow(x, y) {
+    
+    let result = 1; // Початкове значення результату
+  
+    // Множимо result на x y разів
+    for (let i = 1; i <= y; i++) {
+      result *= x;
+    }
+  
+    console.log(result); // Повертаємо результат
+  }
+
+ pow(2, 4);
